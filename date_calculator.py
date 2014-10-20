5ac569eed9ab2f0fb822be81f33eb24b06b4a3c9
taotaocoule <2443877012@qq.com>
2014-10-20 21:47:27 +0800

    start
    just start

---
+++ date_calculator.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+
+@author: taotaocoule
+"""
+from numpy import *
+import datetime
+def date_calculator(x,y,n,data):
+    date=datetime.date(2014,x,y)
+    da=datetime.date(2014,1,1)
+    lengeth=date-ta
+    
+    
+        
